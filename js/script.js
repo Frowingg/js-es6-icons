3756cf7141d3deba277528ec43158440869de102
Frowingg <edbico0@gmail.com>
2022-06-21 17:09:08 +0200

    bonus 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 const iconContainer = document.getElementById('icon_container');
 const userSelection = document.getElementById('type_select');
 const categories = [];
+let randomColor = generateRandomString(6);
+console.log(randomColor)
+
+function generateRandomString(iLen) {
+    var sRnd = '';
+    var sChrs = "0123456789abcdef";
+    for (var i = 0; i < iLen; i++) {
+      var randomPoz = Math.floor(Math.random() * sChrs.length);
+      sRnd += sChrs.substring(randomPoz, randomPoz + 1);
+    }
+    return sRnd;
+};
 const data = 
 [
 	{
@@ -8,114 +20,131 @@ const data =
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
-	},
+		// color: 'orange'
+        color: '#'+generateRandomString(6)
+    },
 	{
 		name: 'crow',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
-	},
+		// color: 'orange'
+        color: '#'+generateRandomString(6)
+    },
 	{
 		name: 'dog',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
-	},
+		// color: 'orange'
+        color: '#'+generateRandomString(6)
+    },
 	{
 		name: 'dove',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
-	},
+		// color: 'orange'
+        color: '#'+generateRandomString(6)
+    },
 	{
 		name: 'dragon',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
-	},
+		// color: 'orange'
+        color: '#'+generateRandomString(6)
+    },
 	{
 		name: 'horse',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
-	},
+		// color: 'orange'
+        color: '#'+generateRandomString(6)
+    },
 	{
 		name: 'hippo',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
-	},
+		// color: 'orange'
+        color: '#'+generateRandomString(6)
+    },
 	{
 		name: 'fish',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
-	},
+		// color: 'orange'
+        color: '#'+generateRandomString(6)
+    },
 	{
 		name: 'carrot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'green'
-	},
+		// color: 'green'
+	    color: '#'+generateRandomString(6)
+    },
 	{
 		name: 'apple-alt',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'green'
-	},
+		// color: 'green'
+	    color: '#'+generateRandomString(6)
+    },
 	{
 		name: 'lemon',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'green'
-	},
+		// color: 'green'
+	    color: '#'+generateRandomString(6)
+    },
 	{
 		name: 'pepper-hot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'green'
-	},
+		// color: 'green'
+	    color: '#'+generateRandomString(6)
+    },
 	{
 		name: 'user-astronaut',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		// color: 'blue'
+        color: '#'+generateRandomString(6)
 	},
-	{
+    {
 		name: 'user-graduate',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		// color: 'blue'
+        color: '#'+generateRandomString(6)
 	},
-	{
+    {
 		name: 'user-ninja',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
-	},
-	{
+		// color: 'blue'
+        color: '#'+generateRandomString(6)
+	},    
+    {
 		name: 'user-secret',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		// color: 'blue'
+        color: '#'+generateRandomString(6)
 	}
 ];
+    
 
 data.forEach(icon => {
     if (!categories.includes(icon.type)) {
@@ -128,7 +157,7 @@ data.forEach(icon => {
     `
 }
 );
-console.log(categories)
+
 categories.forEach(category => {
     userSelection.innerHTML += `<option value="${category}">${category}</option>`;
 });
